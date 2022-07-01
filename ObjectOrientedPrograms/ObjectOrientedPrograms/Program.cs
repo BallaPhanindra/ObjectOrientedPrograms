a2e2db1b5db439dfe7d1ce096da7d54faca4f19f
Balla <ballaphanindra@outlook.com>
2022-07-01 14:24:14 +0530

    updating

--- ObjectOrientedPrograms/ObjectOrientedPrograms/Program.cs
+++ ObjectOrientedPrograms/ObjectOrientedPrograms/Program.cs
@@ -12,14 +12,15 @@ namespace ObjectOrientedPrograms
         {
             Console.WriteLine("Enter 1 for AddressBook\n" +
                 "Enter 2 for InventoryManagement\n" +
-                "Enter 3 For Stock Management\n" 
+                "Enter 3 For Stock Management\n" +
+                "Enter 4 for ommercial Data Processing\n"
                 );
             int number = int.Parse(Console.ReadLine());
 
             switch (number)
             {
-                case 3:
-                    StocksManagement.DriverMethod();
+                case 4:
+                    AccountManagements.DriverMethod();
                     break;
                     default:
                     Console.WriteLine("Invalid Entry");
