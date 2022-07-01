b39cb88bcbdd46c553c9b8a67e298a5f8c93fb07
Balla <ballaphanindra@outlook.com>
2022-07-01 14:34:24 +0530

    updating Program.cs of CDP

--- ObjectOrientedPrograms/ObjectOrientedPrograms/Program.cs
+++ ObjectOrientedPrograms/ObjectOrientedPrograms/Program.cs
@@ -1,26 +1,25 @@
 ﻿using System;
-using System.IO;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 
 namespace ObjectOrientedPrograms
 {
-    public class Program
+public class Program
+{
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            Console.WriteLine("Enter 0 for InventoryManagement\n");
-            int number = int.Parse(Console.ReadLine());
+        Console.WriteLine(
+            "Enter 4 for Account Management\n" );
+        int number = int.Parse(Console.ReadLine());
 
-            switch (number)
-            {
-                case 0:
-                    AccountManagement.DriverMethod();
-                    break;
-                default:
-                    Console.WriteLine("Invalid Entry");
-                    break;
-            }
+        switch (number)
+        {
+            case 4:
+                AccountManagement.DriverMethod();
+                break;
+           
+            default:
+                Console.WriteLine("Invalid Entry");
+                break;
+        }
         }
     }
 }
