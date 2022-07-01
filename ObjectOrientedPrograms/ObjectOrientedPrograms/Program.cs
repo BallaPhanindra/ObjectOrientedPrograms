8b354744f07cab60ea203fb21fcb9ef13c94f9f9
Balla <ballaphanindra@outlook.com>
2022-07-01 12:47:44 +0530

    Committing StocksManagement

--- ObjectOrientedPrograms/ObjectOrientedPrograms/Program.cs
+++ ObjectOrientedPrograms/ObjectOrientedPrograms/Program.cs
@@ -1,25 +1,30 @@
-﻿using System;
-using System.IO;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
+﻿    using System;
+    using System.IO;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Linq;
 
 namespace ObjectOrientedPrograms
-{
+{ 
     public class Program
     {
+
         public static void Main(string[] args)
         {
-            Console.WriteLine("Enter 0 for InventoryManagement\n");
+            Console.WriteLine("Enter 1 for AddressBook\n" +
+                "Enter 2 for InventoryManagement\n" +
+                "Enter 3 For Stock Management\n" 
+                );
             int number = int.Parse(Console.ReadLine());
 
             switch (number)
             {
-                case 0:
-                    InventoryManagement.DiverMethod();
+                case 3:
+                    StocksManagement.DriverMethod();
                     break;
-                default:
+                    default:
                     Console.WriteLine("Invalid Entry");
                     break;
+                
             }
         }
     }
