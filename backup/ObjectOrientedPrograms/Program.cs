b69a168e089ae26b0694382d4d1b7f6e7caead16
Balla <ballaphanindra@outlook.com>
2022-07-01 13:45:26 +0530

    Committing CDP again

---
+++ backup/ObjectOrientedPrograms/Program.cs
@@ -0,0 +1,31 @@
+﻿    using System;
+    using System.IO;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Linq;
+
+namespace ObjectOrientedPrograms
+{ 
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter 1 for AddressBook\n" +
+                "Enter 2 for InventoryManagement\n" +
+                "Enter 3 for Stock Management\n" +"Enter 4 for Commercial Data Processing \n"
+                );
+            int number = int.Parse(Console.ReadLine());
+
+            switch (number)
+            {
+                    case 4:
+                    AccountManagement.DriverMethod();
+                    break;
+                    default:
+                    Console.WriteLine("Invalid Entry");
+                    break;
+                
+            }
+        }
+    }
+}
