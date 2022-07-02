6d1befc2d2a941889b6cdcd27171b84192a8f166
Balla <ballaphanindra@outlook.com>
2022-07-02 21:44:34 +0530

    Updating DeckOfCards

---
+++ ObjectOrientedPrograms/ObjectOrientedPrograms/Card.cs
@@ -0,0 +1,21 @@
+﻿
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+namespace ObjectOrientedPrograms
+{
+    public class Card
+    {
+        private string suit, value;
+        public Card(string tvalue, string tsuit)
+        {
+            this.suit = tsuit;
+            this.value = tvalue;
+        }
+        public override string ToString()
+        {
+            return this.value + " of " + this.suit;
+        }
+    }
+}
--- ObjectOrientedPrograms/ObjectOrientedPrograms/Program.cs
+++ ObjectOrientedPrograms/ObjectOrientedPrograms/Program.cs
@@ -1,31 +1,17 @@
-﻿    using System;
-    using System.IO;
-    using Newtonsoft.Json;
-    using Newtonsoft.Json.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace ObjectOrientedPrograms
-{ 
+{
     public class Program
     {
-
-        public static void Main(string[] args)
+        public static void Main()
         {
-            Console.WriteLine("Enter 1 for AddressBook\n" +
-                "Enter 2 for InventoryManagement\n" +
-                "Enter 3 For Stock Management\n" 
-                );
-            int number = int.Parse(Console.ReadLine());
-
-            switch (number)
-            {
-                case 3:
-                    StocksManagement.DriverMethod();
-                    break;
-                    default:
-                    Console.WriteLine("Invalid Entry");
-                    break;
-                
-            }
+            CardsSimulation simulation = new CardsSimulation();
+            simulation.DriverMethod();
         }
     }
 }
